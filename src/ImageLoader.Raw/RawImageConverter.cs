983d43fcba0b82acdc178f92121d407f17dc467e
Mark Ridgwell <credfeto@users.noreply.github.com>
2018-08-22 23:52:13 +0100

    Removed dead method

--- src/ImageLoader.Raw/RawImageConverter.cs
+++ src/ImageLoader.Raw/RawImageConverter.cs
@@ -10,7 +10,6 @@ using ImageLoader.Interfaces;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
 using SixLabors.ImageSharp.Processing;
-using SixLabors.ImageSharp.Processing.Transforms;
 
 namespace ImageLoader.Raw
 {
@@ -41,12 +40,6 @@ namespace ImageLoader.Raw
             return Task.FromResult(LoadImageInternal(fileName));
         }
 
-        public Task<Image<Rgba32>> LoadImageAsync(Stream source)
-        {
-            throw new NotSupportedException();
-        }
-
-        
 
         private static Image<Rgba32> LoadImageInternal(string filename)
         {
@@ -112,7 +105,7 @@ namespace ImageLoader.Raw
             };
         }
     }
-    
+
     internal static class ConverterCommon
     {
         /// <summary>
@@ -124,53 +117,51 @@ namespace ImageLoader.Raw
         /// <returns>
         ///     The image that was contained in the stream.
         /// </returns>
-        [SuppressMessage(category: "Microsoft.Usage", checkId: "CA1801:ReviewUnusedParameters", MessageId = "fileName", Justification = "Used for logging")]
+        [SuppressMessage("Microsoft.Usage", "CA1801:ReviewUnusedParameters", MessageId = "fileName",
+            Justification = "Used for logging")]
         public static Image<Rgba32> OpenBitmapFromTiffStream(Stream stream)
         {
             Contract.Requires(stream != null);
 
-            using (MemoryStream ms = new MemoryStream())
+            using (var ms = new MemoryStream())
             {
                 stream.CopyTo(ms);
                 ms.Seek(0, SeekOrigin.Begin);
 
-                using (Tiff tif = Tiff.ClientOpen(name: "in-memory", mode: "r", ms, new TiffStream()))
+                using (var tif = Tiff.ClientOpen("in-memory", "r", ms, new TiffStream()))
                 {
                     // Find the width and height of the image
-                    FieldValue[] value = tif.GetField(TiffTag.IMAGEWIDTH);
-                    int width = value[0]
+                    var value = tif.GetField(TiffTag.IMAGEWIDTH);
+                    var width = value[0]
                         .ToInt();
 
                     value = tif.GetField(TiffTag.IMAGELENGTH);
-                    int height = value[0]
+                    var height = value[0]
                         .ToInt();
 
                     // Read the image into the memory buffer
-                    int[] raster = new int[height * width];
+                    var raster = new int[height * width];
 
-                    if (!tif.ReadRGBAImage(width, height, raster))
-                    {
-                        return null;
-                    }
+                    if (!tif.ReadRGBAImage(width, height, raster)) return null;
 
-                    bool ok = false;
+                    var ok = false;
                     Image<Rgba32> bmp = null;
 
                     try
                     {
                         bmp = new Image<Rgba32>(width, height);
 
-                        for (int y = 0; y < bmp.Height; y++)
+                        for (var y = 0; y < bmp.Height; y++)
                         {
-                            int rasterOffset = y * bmp.Width;
+                            var rasterOffset = y * bmp.Width;
 
-                            for (int x = 0; x < bmp.Width; x++)
+                            for (var x = 0; x < bmp.Width; x++)
                             {
-                                int rgba = raster[rasterOffset++];
-                                byte r = (byte) ((rgba >> 16) & 0xff);
-                                byte g = (byte) ((rgba >> 8) & 0xff);
-                                byte b = (byte) (rgba & 0xff);
-                                byte a = (byte) ((rgba >> 24) & 0xff);
+                                var rgba = raster[rasterOffset++];
+                                var r = (byte) ((rgba >> 16) & 0xff);
+                                var g = (byte) ((rgba >> 8) & 0xff);
+                                var b = (byte) (rgba & 0xff);
+                                var a = (byte) ((rgba >> 24) & 0xff);
 
                                 bmp[x, y] = new Rgba32(r, g, b, a);
                             }
@@ -181,12 +172,11 @@ namespace ImageLoader.Raw
                     }
                     finally
                     {
-                        if(!ok)
-                        bmp?.Dispose();
-
+                        if (!ok)
+                            bmp?.Dispose();
                     }
                 }
             }
-       }
+        }
     }
 }
