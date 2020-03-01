910b8afc8afeef46506114938825fd3b8b019ec8
Mark Ridgwell <credfeto@users.noreply.github.com>
2020-03-01 15:29:45 +0000

    attempted to load file using assembly path

--- src/ImageLoader.Raw.UnitTests/CanonRawTests.cs
+++ src/ImageLoader.Raw.UnitTests/CanonRawTests.cs
@@ -1,10 +1,12 @@
 using System;
+using System.IO;
 using System.Threading.Tasks;
 using ImageLoader.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
 using Xunit;
+using Xunit.Sdk;
 
 namespace ImageLoader.Raw.UnitTests
 {
@@ -24,6 +26,22 @@ namespace ImageLoader.Raw.UnitTests
 
         private readonly IImageConverter _converter;
 
+        private string FindTestFile(string fileName)
+        {
+            string? location = Path.GetDirectoryName(typeof(CanonRawTests).Assembly.Location);
+
+            if (location == null)
+            {
+                throw new NullException($"Could not fine {fileName}");
+            }
+
+            string file = Path.Combine(location, fileName);
+
+            Assert.True(File.Exists(file), $"Could not fine {fileName} at {file}");
+
+            return file;
+        }
+
         [Fact]
         public void Cr2ExtensionSupported()
         {
@@ -33,7 +51,9 @@ namespace ImageLoader.Raw.UnitTests
         [Fact]
         public async Task LoadCr2Async()
         {
-            Image<Rgba32> image = await this._converter.LoadImageAsync(fileName: @"test.cr2");
+            string fileName = this.FindTestFile(fileName: @"test.cr2");
+
+            Image<Rgba32> image = await this._converter.LoadImageAsync(fileName);
 
             Assert.NotNull(image);
         }
